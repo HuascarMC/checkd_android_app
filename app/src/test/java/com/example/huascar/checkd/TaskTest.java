59aece57758a181da7e74739c47f2456c55187cb
Huascar <huascarmc@hotmail.com>
2017-11-17 16:06:21 +0000

    added test file

---
+++ app/src/test/java/com/example/huascar/checkd/TaskTest.java
@@ -0,0 +1,15 @@
+package com.example.huascar.checkd;
+
+import org.junit.Before;
+
+/**
+ * Created by huascar on 17/11/2017.
+ */
+
+public class TaskTest {
+
+    @Before
+    public void before() {
+        Task task = new Task("Go shopping", "Go to tesco necxt Saturday 3pm", false);
+    }
+}
