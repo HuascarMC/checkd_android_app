0523cc8fceaaf6088f2fe807eb9a426c33a8b3c3
Huascar <huascarmc@hotmail.com>
2017-11-19 16:11:14 +0000

    can access database from mainActivity

--- app/src/main/java/com/example/huascar/checkd/MainActivity.java
+++ app/src/main/java/com/example/huascar/checkd/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.huascar.checkd;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDBHelper = new DatabaseHelper(this);
+        mDatabase  = mDBHelper.getWritableDatabase();
+        mDatabase.close();
     }
 }
