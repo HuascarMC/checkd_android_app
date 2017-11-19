e632881d5e76d7f9a2408ed982bfe5b30ad6f1c9
Huascar <huascarmc@hotmail.com>
2017-11-19 14:39:58 +0000

    added drop table onCreate

--- app/src/main/java/com/example/huascar/checkd/DatabaseHelper.java
+++ app/src/main/java/com/example/huascar/checkd/DatabaseHelper.java
@@ -34,7 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("");
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
