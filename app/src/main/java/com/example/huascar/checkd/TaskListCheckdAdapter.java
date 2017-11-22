958fd777c485b4910bc9332309af3fbfc61fb0bd
Huascar <huascarmc@hotmail.com>
2017-11-22 14:05:33 +0000

    prepared checkdadapter class

--- app/src/main/java/com/example/huascar/checkd/TaskListCheckdAdapter.java
+++ app/src/main/java/com/example/huascar/checkd/TaskListCheckdAdapter.java
@@ -1,8 +1,57 @@
 package com.example.huascar.checkd;
 
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.huascar.checkd.models.Task;
+
+import java.util.ArrayList;
 /**
  * Created by huascar on 22/11/2017.
  */
 
-public class TaskListCheckdAdapter {
+public class TaskListCheckdAdapter extends ArrayAdapter{
+
+    private TextView viewDesc;
+    private TextView titleInput;
+    private TextView taskDesc;
+    private TextView completedBox;
+
+    public TaskListCheckdAdapter(Context context, ArrayList<Task> tasks) {
+        super(context, 0, tasks);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent) {
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.activity_task_list, parent, false);
+
+        }
+
+        Task currentTaskItem = (Task) getItem(position);
+
+        TextView title = listItemView.findViewById(R.id.title);
+        title.setText(currentTaskItem.getTitle());
+
+        TextView description = listItemView.findViewById(R.id.desc);
+        description.setText(currentTaskItem.getDescription());
+
+        Button viewMore = listItemView.findViewById(R.id.button2);
+        viewMore.setTag(currentTaskItem);
+
+        Button delete = listItemView.findViewById(R.id.delete);
+        delete.setTag(currentTaskItem);
+        // this line of code is to be picked up by the click event
+//        listItemView.setTag(currentTaskItem);
+
+        return listItemView;
+    }
+
 }
